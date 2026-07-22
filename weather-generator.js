0ecd6322ba13b434ef56b6f86cb36583afe72c1b
EliAndino <69528724+eliandino@users.noreply.github.com>
2026-07-22 14:10:47 -0400

    Change console log message in weather-generator.js

--- weather-generator.js
+++ weather-generator.js
@@ -7,7 +7,7 @@
 // https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.min.js
 // ============================================================
 
-console.log("Weather Generator loaded from GitHub");
+console.log("Hey man! good job,you are making progress!👍");
 
 const $ = (selector) => document.querySelector(selector);
 
